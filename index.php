c90982e58554d22dbae5e5a6627464d6b8091d75
Erik-Hermawan <erick.hermawan88@gmail.com>
2020-12-19 04:01:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
     <h1>hello wolrd
     </h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit saepe id, fuga laudantium obcaecati, eveniet sit excepturi vero unde explicabo nam maxime, atque doloribus beatae. Mollitia reprehenderit inventore corrupti voluptatum?</p>
+    <ul>
+        <li>satu</li>
+    </ul>
 </body>
 </html>
